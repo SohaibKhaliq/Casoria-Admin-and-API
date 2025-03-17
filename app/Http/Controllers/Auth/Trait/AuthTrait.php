169b421d6ae687fe16eca4360eaf2e5f4b41b152
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-03-17 21:50:42 +0500

    Add mobile field to user registration and improve code formatting

--- app/Http/Controllers/Auth/Trait/AuthTrait.php
+++ app/Http/Controllers/Auth/Trait/AuthTrait.php
@@ -34,13 +34,14 @@ trait AuthTrait
             'last_name' => ['required', 'string', 'max:191'],
             'email' => ['required', 'string', 'email', 'max:191', 'unique:users'],
             'password' => ['required', Rules\Password::defaults()],
+            'mobile' => ['nullable', 'string', 'max:15', 'unique:users'],
             'gender' => ['nullable'],
         ]);
 
         $arr = [
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
-            'name' => $request->first_name.' '.$request->last_name,
+            'name' => $request->first_name . ' ' . $request->last_name,
             'email' => $request->email,
             'mobile' => $request->mobile,
             'gender' => $request->gender ?? null,
