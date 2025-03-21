53b6af8cec51d4e4b988141f2c6bed877c9bade7
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-03-21 22:50:48 +0500

    Add API route to retrieve all pages with authentication

--- Modules/Page/routes/api.php
+++ Modules/Page/routes/api.php
@@ -1,1 +1,9 @@
 <?php
+// This file contains the API routes for the Page module.
+
+use Illuminate\Support\Facades\Route;
+use Modules\Page\Http\Controllers\Backend\PageController;
+
+// Route to get all pages with auth middleware
+
+Route::get('pages', [PageController::class, 'index']);
